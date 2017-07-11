a281e232e9c1fc19c0854df43a4f84db22849b55
lorenzodianni <lorenzodianni@gmail.com>
2017-07-11 12:49:57 +0200

    refactor(component): sidebar-js

--- src/lib/src/sidebarjs/sidebarjs.component.spec.ts
+++ src/lib/src/sidebarjs/sidebarjs.component.spec.ts
@@ -2,32 +2,24 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { LibComponent } from './lib.component';
+import { SidebarJSComponent } from './sidebarjs.component';
 
-describe('LibComponent', function () {
+describe('SidebarJSComponent', function () {
   let de: DebugElement;
-  let comp: LibComponent;
-  let fixture: ComponentFixture<LibComponent>;
+  let comp: SidebarJSComponent;
+  let fixture: ComponentFixture<SidebarJSComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LibComponent]
+      declarations: [SidebarJSComponent]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LibComponent);
+    fixture = TestBed.createComponent(SidebarJSComponent);
     comp = fixture.componentInstance;
-    de = fixture.debugElement.query(By.css('h2'));
   });
 
   it('should create component', () => expect(comp).toBeDefined());
-
-  it('should have expected <h2> text', () => {
-    fixture.detectChanges();
-    const h2 = de.nativeElement;
-    expect(h2.innerText).toMatch(/angular/i,
-      '<h2> should say something about "Angular"');
-  });
 });
