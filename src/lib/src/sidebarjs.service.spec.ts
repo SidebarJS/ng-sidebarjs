52591ac925eb98b6ef11141a5b09d9af38a31e04
lorenzodianni <lorenzodianni@gmail.com>
2017-07-12 17:27:15 +0200

    test(service): SidebarJSService

---
+++ src/lib/src/sidebarjs.service.spec.ts
@@ -0,0 +1,95 @@
+import { TestBed } from '@angular/core/testing';
+import { SidebarJSService } from './sidebarjs.service';
+import * as SidebarJS from 'sidebarjs';
+
+const createSidebar = (sidebarName: string = '') => {
+  const component = document.createElement('div');
+  const container = document.createElement('div');
+  const background = document.createElement('div');
+  component.setAttribute('sidebarjs', sidebarName);
+  container.setAttribute('sidebarjs-container', '');
+  background.setAttribute('sidebarjs-background', '');
+  component.appendChild(container);
+  component.appendChild(background);
+  return {component, container, background};
+};
+
+describe('SidebarJSService', () => {
+  let service: SidebarJSService;
+
+  beforeEach(() => {
+    const bed = TestBed.configureTestingModule({
+      providers: [SidebarJSService]
+    });
+    service = bed.get(SidebarJSService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should be init', () => {
+    const instance = service.init(createSidebar());
+    expect(instance instanceof SidebarJS).toBeTruthy();
+    expect(Object.keys(service['instances']).length).toBe(1);
+    const instanceNamed = service.init(createSidebar('testName'));
+    expect(instanceNamed instanceof SidebarJS).toBeTruthy();
+    expect(Object.keys(service['instances']).length).toBe(2);
+    expect(service['instances'].testName).toBeDefined();
+  });
+
+  it('should be opened', () => {
+    const sidebarName = 'testOpen';
+    service.init(createSidebar(sidebarName));
+    service.open(sidebarName);
+    expect(service['instances'][sidebarName]).toBeDefined();
+    expect(service.isVisible(sidebarName)).toBe(true);
+  });
+
+  it('should be closed', () => {
+    const sidebarName = 'testClose';
+    service.init(createSidebar(sidebarName));
+    service.open(sidebarName);
+    expect(service.isVisible(sidebarName)).toBe(true);
+    service.close(sidebarName);
+    expect(service.isVisible(sidebarName)).toBe(false);
+  });
+
+  it('should be toggled', () => {
+    const sidebarName = 'testToggle';
+    service.init(createSidebar(sidebarName));
+    service.toggle(sidebarName);
+    expect(service.isVisible(sidebarName)).toBe(true);
+    service.toggle(sidebarName);
+    expect(service.isVisible(sidebarName)).toBe(false);
+    service.toggle(sidebarName);
+    expect(service.isVisible(sidebarName)).toBe(true);
+  });
+
+  it('should change position', () => {
+    const instance = service.init(createSidebar());
+    expect(instance.position).toBe('left');
+    service.setPosition('right');
+    expect(instance.position).toBe('right');
+  });
+
+  it('should check/set listener property', () => {
+    const div = document.createElement('div');
+    expect(service.elemHasListener(div)).toBe(false);
+    service.elemHasListener(div, true);
+    expect(service.elemHasListener(div)).toBe(true);
+    service.elemHasListener(div, false);
+    expect(service.elemHasListener(div)).toBe(false);
+  });
+
+  it('should destroy instance', () => {
+    const sidebarName = 'testDestroy';
+    expect(Object.keys(service['instances']).length).toBe(0);
+    service.init(createSidebar(sidebarName));
+    expect(service['instances'][sidebarName]).toBeDefined();
+    expect(Object.keys(service['instances']).length).toBe(1);
+    service.destroy(sidebarName);
+    expect(service['instances'][sidebarName]).toBeUndefined();
+    expect(Object.keys(service['instances']).length).toBe(0);
+  });
+});
