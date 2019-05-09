d316ebe3901ca04b7a75f9694ac6f8af6df6f31d
Lorenzo D'Ianni <lorenzodianni@gmail.com>
2019-05-10 01:08:47 +0200

    feat(lib): SidebarjsToggleDirective

--- projects/ng-sidebarjs/src/lib/sidebarjs-toggle.directive.ts
+++ projects/ng-sidebarjs/src/lib/sidebarjs-toggle.directive.ts
@@ -1,23 +1,23 @@
 import { Directive, ElementRef, Input, OnDestroy, Renderer2 } from '@angular/core';
-import { HTMLSidebarElement, SidebarService } from 'sidebarjs';
+import { SidebarjsService, HTMLSidebarElement } from './sidebarjs.service';
 
 @Directive({
   selector: '[sidebarjsToggle]',
 })
-export class SidebarToggleDirective implements OnDestroy {
+export class SidebarjsToggleDirective implements OnDestroy {
   @Input() sidebarjsToggle = '';
 
   private readonly destroyListener = this.renderer.listen(
     this.elementRef.nativeElement,
     'click',
     ({target}: Event) => {
-      if (!this.sidebarService.elemHasListener(target as HTMLSidebarElement)) {
-        this.sidebarService.toggle(this.sidebarjsToggle);
+      if (!this.sidebarjsService.elemHasListener(target as HTMLSidebarElement)) {
+        this.sidebarjsService.toggle(this.sidebarjsToggle);
       }
     });
 
   constructor(
-    private readonly sidebarService: SidebarService,
+    private readonly sidebarjsService: SidebarjsService,
     private readonly renderer: Renderer2,
     private readonly elementRef: ElementRef<Element>,
   ) {
